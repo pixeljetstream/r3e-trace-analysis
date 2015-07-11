12c63cc06591799a12bc14ac5856fa7aa72cdbc4
Christoph Kubisch <pixeljetstream@luxinia.de>
2015-07-11 21:51:52 +0200

    automatic access table to all properties

--- r3e-trace-replay.lua
+++ r3e-trace-replay.lua
@@ -2,14 +2,16 @@ local ffi          = require "ffi"
 local r3e          = require "r3e"
 local r3etrace     = require "r3etrace"
 local utils        = require "utils"
+local r3emap       = require "r3emap"
 local state        = ffi.new( r3e.SHARED_TYPE )
 
 local args = _ARGS or {...}
 
-local traceFileName = args[2] or "trace_test.r3t"
+local traceFileName = args[2] or "trace_150711_183527.r3t"
 
 local trace = r3etrace.loadTrace(traceFileName)
 
+local dumpinterval = 2
 local playspeed    = 1
 local playbackrate = math.max(1,math.floor(trace.pollrate/2))
 
@@ -36,10 +38,12 @@ local lastInterval = nil
 while (time <= timeEnd) do
   update()
   
-  -- dump every second
-  local interval = math.floor(time)
+  -- dump every once and a while
+  local interval = math.floor(time/dumpinterval)
   if (interval ~= lastInterval) then
-    print("time",time, "pos", state.Player.Position.X, state.Player.Position.Z)
+    print("PLAYER TIME",time)
+    r3emap.printAll(state)
+    print ""
     lastInterval = interval
   end
   
